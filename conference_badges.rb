71b7732aeeb69e9194f336706dc8213a61178229
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-05-03 17:47:25 -0400

    badges and schedules solution

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_array = []
+  names.each do |name|
+    badge_array << badge_maker(name)
+  end
+  return badge_array
+end
+
+
+def assign_rooms(names)
+  rooms_array = []
+  names.each_with_index do |name,i|
+    rooms_array << "Hello, #{name}! You'll be assigned to room #{i+1}!"
+  end
+  return rooms_array
+end
+
+def printer(names)
+    badge_array = batch_badge_creator(names)
+    rooms_array = assign_rooms(names)
+    badge_array.each do |name|
+      puts name
+    end
+    rooms_array.each do |name|
+      puts name
+    end
+end
